19620361282e039524390c9aee5997649f9f9959
stas <stas@vlad-2.local>
2018-12-13 23:52:50 +0300

    added header component

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="main">
+        <div class="wrapper">
+            <div class="content">
+            <div class="text">
+                <div class="heading">
+                    <p class="head">Шкодинг</p>
+                </div>
+                <div class="description">
+                    <p class="description-text"> Онлайн-уроки кодинга 
+                        для детей или родителей 
+                    </p>
+                </div>
+            </div>
+            <div class="icon">
+                <p class="emoji">🚀</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.wrapper{
+    background-color: #f7d355;
+    height: 424px;
+    line-height: 1px; 
+}
+.content{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    position: relative;
+    margin-left: 66px;
+    top: 87px;
+}
+.text{
+    
+     max-width: 490px;
+}
+.head{
+    font-size: 90px;
+    line-height: 112px;
+    text-align: left;
+    letter-spacing: 6px;
+    font-weight: bold;
+}
+.description-text{
+    font-weight: 500;
+    line-height: 45px;
+    font-size: 40px;
+    text-align: left;
+    letter-spacing: 0.6px;
+}
+.icon{
+    
+}
+.emoji{
+    letter-spacing: 3px;
+    font-weight: 500;
+    line-height: 225px;
+    font-size: 180px;
+    text-align: center;
+    width: 279px;
+    height: 219px;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,30 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        mayak-coding
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <div class="main">
+    <div class="wrapper">
+      <TheHeader/>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+
+import TheHeader from '~/components/TheHeader/TheHeader.vue'
+require('@/components/reset.css')
 
 export default {
   components: {
-    AppLogo
+    TheHeader
   }
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+.wrapper{
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+TheHeader{
+  height: 424px;
+  width: 100%;
 }
 </style>
 
